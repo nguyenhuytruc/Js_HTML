5bceb5dbb2204a4f3f4d8294cb662dcd2c12f9d1
nguyenhuytruc <huytruc1201@gmail.com>
2022-06-13 16:13:53 +0700

    diem trung binh

---
+++ diemtrungbinh.js
@@ -0,0 +1,7 @@
+var physics = 7;
+var chemistry = 8;
+var biological = 8;
+var DTB = (physics + chemistry + biological)/3;
+var total = physics + chemistry + biological;
+console.log(DTB);
+console.log(total);
