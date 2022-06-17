4fefd2ed5a24682838beb9cfbfe2976819d4b306
nguyenhuytruc <huytruc1201@gmail.com>
2022-06-17 14:25:11 +0700

    vong lap for

---
+++ tinhtiendien.js
@@ -0,0 +1,17 @@
+function process() {
+    let dntt = Number(document.getElementById('dntt').value);
+}
+let b1 = 0;
+let b2 = 0;
+let b3 = 0;
+let b4 = 0;
+let b5 = 0;
+let b6 = 0;
+let tong = 0;
+switch (true) {
+    case dntt > 400: {
+        b1 = (50 * 1678);
+        break;
+    }
+    case dntt 
+}
